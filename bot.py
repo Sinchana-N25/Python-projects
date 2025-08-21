b82d3995f8301ad64ecd4e4c51ee499d276b0c4f
Sinchana N <sinchana2505@gmail.com>
2025-08-21 10:47:45 +0530

    discord bot for generating memes from reddit

---
+++ bot.py
@@ -0,0 +1,32 @@
+import discord
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# update the url to https://meme-api.com/gimme
+def get_meme():
+    response = requests.get('https://meme-api.com/gimme')
+    json_data = json.loads(response.text)
+    return json_data['url']
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
+
+# create intents keyword argument
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(TOKEN) # Replace with your own token.
